fe33c4208382549922f98dcc975e9b52700d3383
mrlrsx <lucas.rocha-dev@outlook.com>
2023-10-28 16:57:52 -0300

    or condição

---
+++ aula22.py
@@ -0,0 +1,15 @@
+print('teste')
+print(' -> and ', 1 and 1)
+print(' -> or ', 0 or 1)
+print(' -> and ',0 or 0 )
+print(' -> and ',0 and 0 )
+
+
+if 0 and 1:
+     print('1 condição')
+elif (0 and 0) == False:
+     print('2 condição')
+elif 1 and 1:
+     print('3 condição')
+else:
+     print('Pode isso arnaldo !!! ')
