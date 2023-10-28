d468378777ae1a3e10accfb61623d214088a6b86
mrlrsx <lucas.rocha-dev@outlook.com>
2023-10-28 13:34:22 -0300

    concatenação

---
+++ aula10.py
@@ -0,0 +1,17 @@
+concatenacao = 'Lucas' + ' ' + 'Rocha'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_luiz = 3 * 'Lucas'
+print(a_dez_vezes)
+print(tres_vezes_luiz, end="\n")
+l_vinte_vezes = 'L' * 20
+print(l_vinte_vezes)
+
+print(25 % 5)
+print(25 // 4) 
+print(25 / 2)
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print('E par ou impar? ', 10 % 2 == 0)
+print('E par ou impar? ', 10 % 3 == 0)
