32619af9bba5e6ee6c5849ccb146a236f18c47e0
mrlrsx <lucas.rocha-dev@outlook.com>
2023-10-28 14:44:44 -0300

    funçao input

--- aula14.py
+++ aula14.py
@@ -8,3 +8,22 @@ formato = string.format(
 
 print(formato)
 print(string)
+
+
+nome = "Luiz"
+idade = 23
+formato = '{1} tem {0} anos'
+print(formato.format(nome, idade))
+
+formato = '{n} tem {i} anos'
+print(formato.format(n=nome, i=idade))
+
+formato = f'{nome} tem {idade:.2f} anos'
+print(formato)
+
+numero_1 = 10
+numero_2 = 20
+resultado = numero_1 * numero_2
+print(resultado)
+
+print("lucas"+1)
