db3df5364904fda14657b5d3260c6a9cb019475d
mrlrsx <lucas.rocha-dev@outlook.com>
2023-10-28 15:02:15 -0300

    correção

--- aula14.py
+++ aula14.py
@@ -1,6 +1,8 @@
 a = 'AAAAA'
 b = 'BBBBBB'
 c = 1.1
+
+#parametro nomeado variavel na string b={nome2} no method format recebe o valor de b
 string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
 formato = string.format(
     nome1=a, nome2=b, nome3=c
@@ -9,7 +11,7 @@ formato = string.format(
 print(formato)
 print(string)
 
-
+#indices lembrando a ordem do method
 nome = "Luiz"
 idade = 23
 formato = '{1} tem {0} anos'
@@ -25,5 +27,3 @@ numero_1 = 10
 numero_2 = 20
 resultado = numero_1 * numero_2
 print(resultado)
-
-print("lucas"+1)
--- aula15.py
+++ aula15.py
@@ -8,3 +8,5 @@ int_numero_1 = int(numero_1)
 int_numero_2 = int(numero_2)
 
 print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
+nome = input('digite seu nome? ')
+print(f'O seu nome é {nome} ')
