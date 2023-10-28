e28a81c2db75b08c41157d80c650863aa4646fe3
mrlrsx <lucas.rocha-dev@outlook.com>
2023-10-28 15:07:48 -0300

    if else

--- aula8.py
+++ aula8.py
@@ -11,4 +11,5 @@ print('Idade:', idade)
 print('Ano de nascimento:', ano_nascimento)
 print('É maior de idade?', maior_de_idade)
 print('Altura em metros:', altura_metros)
+
 print(type(nome), type(sobrenome), type(ano_nascimento), type(maior_de_idade), type(altura_metros), sep=" : ")
