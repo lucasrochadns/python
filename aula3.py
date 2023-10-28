2abd035b6550b54fbe614e42e5505b53fcab9af0
mrlrsx <lucas.rocha-dev@outlook.com>
2023-10-28 10:47:30 -0300

    Teste caracter escape

---
+++ aula3.py
@@ -0,0 +1,12 @@
+#Tipos de dados
+#Tipagem dinamica.... 
+
+print('Lucas Rocha')
+
+#escape
+print("Lucas \"Rocha\" ")
+
+#r exibe caracter de escape
+print(r"Lucas \"Rocha ")
+
+print(' "Lucas Rocha" ')
