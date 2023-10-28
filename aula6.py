d9fe51700ae1203843d4b8d42db2bdaee15865cb
mrlrsx <lucas.rocha-dev@outlook.com>
2023-10-28 13:21:36 -0300

    var

--- aula6.py
+++ aula6.py
@@ -7,3 +7,9 @@ print(int('1'), type(int('1')))
 print(type(float('1') + 1))
 print(bool(' '))
 print(str(11) + 'b')
+
+print(int('100') * 10)
+print('Lucas Rocha ' + str('28'))
+idade = 28
+maiorQue = idade > 18
+print('E de maior? ', maiorQue, sep=":") 
