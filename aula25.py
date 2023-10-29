8f487f5b313483fa1b6f8e2053b54af08d06c11d
mrlrsx <lucas.rocha-dev@outlook.com>
2023-10-29 14:10:44 -0300

    exercicios

--- aula25.py
+++ aula25.py
@@ -1,13 +1,13 @@
 # Operadores in e not in
 # Strings são iteráveis
 #  0 1 2 3 4 5
-#  O t á v i o
+#  L U C A S rR
 # -6-5-4-3-2-1
 nome = 'Lucas'
 print(nome[2])
 print(nome[-4])
-print('vio' in nome)
-print('zero' in nome)
+print('as' in nome)
+print('san' in nome)
 print(10 * '-')
 print('vio' not in nome)
 print('zero' not in nome)
---
+++ aula27.py
@@ -0,0 +1,6 @@
+nome = 'Maria Carmo'
+ 
+if ' ' in nome:
+    print(f'O nome {nome} tem espaços.')
+else:
+    print(f'O nome {nome} NÃO tem espaços.')
