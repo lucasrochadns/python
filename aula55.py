6eecbff32c0e0646533c2cb21f4d4e746fad55f3
mrlrsx <lucas.rocha-dev@outlook.com>
2023-10-30 16:47:53 -0300

    teste

--- aula55.py
+++ aula55.py
@@ -2,6 +2,6 @@ lista = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 for exemplo in range(len(lista)):
     lista[exemplo] += 20
-    
+
 for exemplo in lista:
     print(exemplo)
---
+++ aula57.py
@@ -0,0 +1,5 @@
+lista = [10, 20, 30, 40]
+lista.insert(1, 100)
+
+for exemplo in lista:
+    print(exemplo)
