158278d156c52b030d5d241a083d904457aa28cd
mrlrsx <lucas.rocha-dev@outlook.com>
2023-10-30 13:04:33 -0300

    teste

---
+++ aula39.py
@@ -0,0 +1,8 @@
+#Objetos imutaveis int float bool str
+#não se pode alterar o valor 
+
+string = 'lucas rocha'
+outra_variavel = f'{string[:3]}SSS{string[:4]}'
+
+print(outra_variavel)
+print(string.capitalize())
