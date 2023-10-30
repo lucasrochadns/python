d8945f567d177883bb70c29411c14de59626b159
mrlrsx <lucas.rocha-dev@outlook.com>
2023-10-30 11:35:26 -0300

    exercicio

---
+++ aula36.py
@@ -0,0 +1,5 @@
+variavel1 = 10
+variavel2 = 20
+
+print(id(variavel2))
+print(id(variavel1))
---
+++ aula37.py
@@ -0,0 +1,9 @@
+condicao = False
+passou_no_if = None
+
+if condicao:
+    print("Faça algo")
+else:
+    print('Não faça algo ')
+
+print(f'Variavel passou_no_if é {passou_no_if is None}')
